a9a37a045a74f9ef2da8d330954c8012a6360064
HJ <uhyeonjin0619@gmail.com>
2022-08-12 23:20:37 +0900

    comment-serializer

--- djangogram/djangogram/posts/serializers.py
+++ djangogram/djangogram/posts/serializers.py
@@ -3,15 +3,6 @@ from rest_framework import serializers
 from djangogram.users.models import User as user_model
 from . import models
 
-class CommentSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = models.Comment
-        fields = (
-            "id",
-            "contents",
-        )
-        
-
 
 class FeedAuthorSerializer(serializers.ModelSerializer):
     class Meta:
@@ -22,6 +13,17 @@ class FeedAuthorSerializer(serializers.ModelSerializer):
             "profile_photo",
         )
 
+
+class CommentSerializer(serializers.ModelSerializer):
+    author = FeedAuthorSerializer()
+    class Meta:
+        model = models.Comment
+        fields = (
+            "id",
+            "comments",
+            "author",
+        )
+        
 
 class PostSerializer(serializers.ModelSerializer):
     comment_post = CommentSerializer(many=True)
